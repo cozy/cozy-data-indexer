1f65014d0aa29fabe271e2d4e4a75d75d364733b
Frank Rousseau <frank.rousseau@cozycloud.cc>
2015-04-06 14:28:12 +0200

    remove weboob from indexer

--- urls.py
+++ urls.py
@@ -1,12 +1,9 @@
 from handlers import indexer
-from handlers import connectors
 
 url_patterns = [
     (r"/$", indexer.VersionHandler),
     (r"/index/$", indexer.IndexHandler),
     (r"/index/(.*)/$", indexer.IndexHandler),
     (r"/search/$", indexer.SearchHandler),
-    (r"/connectors/bank/([0-9a-z]+)/$", connectors.BankHandler),
-    (r"/connectors/bank/([0-9a-z]+)/history/$", connectors.BankHistoryHandler),
     (r"/clear-all/$", indexer.ClearHandler),
 ]
